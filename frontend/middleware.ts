6d873c493b16e3ee4943164b7635dc0cfc738a7c
maywascold <stepanovigor2110@gmail.com>
2024-07-09 15:50:37 +0300

    229

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -1,14 +1,14 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
-import {getMyUserData} from "@/data/services/get-my-user-data";
+import {getMyUserData} from "./data/services/get-my-user-data";
 
 export async function middleware(request: NextRequest) {
-    // const user = await getMyUserData();
-    // const currentPath = request.nextUrl.pathname;
+    const user = await getMyUserData();
+    const currentPath = request.nextUrl.pathname;
 
-    // if (currentPath.startsWith("/dashboard") && user.ok === false) {
-    //     return NextResponse.redirect(new URL("/login", request.url));
-    // }
+    if (currentPath.startsWith("/dashboard") && user.ok === false) {
+        return NextResponse.redirect(new URL("/login", request.url));
+    }
 
-    // return NextResponse.next();
+    return NextResponse.next();
 }
