e10caa23ff6e2b01212f852c029ee165a1794652
maywascold <stepanovigor2110@gmail.com>
2024-07-11 23:27:38 +0300

    add host

--- frontend/data/action/auth-action.ts
+++ frontend/data/action/auth-action.ts
@@ -15,7 +15,7 @@ export interface INewUser {
 const config = {
   maxAge: 60 * 60 * 24 * 7, // 1 week
   path: "/",
-  domain: process.env.HOST ?? "localhost",
+  domain: process.env.NEXT_PUBLIC_HOST ?? "localhost",
   httpOnly: true,
   secure: process.env.NODE_ENV === "production",
 };
